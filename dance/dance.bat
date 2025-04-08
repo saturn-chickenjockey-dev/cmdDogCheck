a2304e143321a079671bcb68b13ef196bc8a2e1a
Saturn <sussycat2133@gmail.com>
2025-04-08 18:20:52 +0100

    Update dance.bat

--- dance/dance.bat
+++ dance/dance.bat
@@ -1,7 +1,9 @@
-@echo off
+@echo on
+cls
 echo WARNING: This script attempts to close every single cmd window after its completion, if you have any work on neovim, a console based IDE, etc.. save your work and prepare for the dog
 echo.
 pause
+cls
 cd %USERPROFILE%\Documents\cmdDogCheck_dancetype
 
 start /min "Amazing Instance of CMD with a purpose." cmd /k "@echo off && echo I am the sound system. Close me after the animation, or else there will be no sound for the animation. && powershell -c (New-Object Media.SoundPlayer "%USERPROFILE%\Documents\cmdDogCheck_dancetype\mus_dance_of_dog.wav").PlaySync(); %% exit"
@@ -159,5 +161,5 @@ cls
 type frame02.txt
 powershell -Command "Start-Sleep -Milliseconds 200"
 cls
+start "handler for ps" cmd /C "timeout /t 1 -nobreak > nul && taskkill /f /im WindowsTerminal.exe"
 taskkill /f /im cmd.exe
-taskkill /f /im WindowsTerminal.exe
