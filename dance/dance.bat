49eff907adda2caf050125efb68f07140a268060
Saturn <sussycat2133@gmail.com>
2025-04-08 18:11:53 +0100

    Add files via upload

---
+++ dance/dance.bat
@@ -0,0 +1,163 @@
+@echo off
+echo WARNING: This script attempts to close every single cmd window after its completion, if you have any work on neovim, a console based IDE, etc.. save your work and prepare for the dog
+echo.
+pause
+cd %USERPROFILE%\Documents\cmdDogCheck_dancetype
+
+start /min "Amazing Instance of CMD with a purpose." cmd /k "@echo off && echo I am the sound system. Close me after the animation, or else there will be no sound for the animation. && powershell -c (New-Object Media.SoundPlayer "%USERPROFILE%\Documents\cmdDogCheck_dancetype\mus_dance_of_dog.wav").PlaySync(); %% exit"
+
+chcp 65001
+cls
+@echo off
+cls
+goto dogframes
+
+
+:dogframes    
+                   
+type frame01.txt
+powershell -Command "Start-Sleep -Milliseconds 200"
+cls
+type frame02.txt
+powershell -Command "Start-Sleep -Milliseconds 200"
+cls
+type frame01.txt
+powershell -Command "Start-Sleep -Milliseconds 200"
+cls
+type frame02.txt
+powershell -Command "Start-Sleep -Milliseconds 200"
+cls
+type frame01.txt
+powershell -Command "Start-Sleep -Milliseconds 200"
+cls
+type frame02.txt
+powershell -Command "Start-Sleep -Milliseconds 200"
+cls
+type frame01.txt
+powershell -Command "Start-Sleep -Milliseconds 200"
+cls
+type frame02.txt
+powershell -Command "Start-Sleep -Milliseconds 200"
+cls
+type frame01.txt
+powershell -Command "Start-Sleep -Milliseconds 200"
+cls
+type frame02.txt
+powershell -Command "Start-Sleep -Milliseconds 200"
+cls
+type frame01.txt
+powershell -Command "Start-Sleep -Milliseconds 200"
+cls
+type frame02.txt
+powershell -Command "Start-Sleep -Milliseconds 200"
+cls
+type frame01.txt
+powershell -Command "Start-Sleep -Milliseconds 200"
+cls
+type frame02.txt
+powershell -Command "Start-Sleep -Milliseconds 200"
+cls
+type frame01.txt
+powershell -Command "Start-Sleep -Milliseconds 200"
+cls
+type frame02.txt
+powershell -Command "Start-Sleep -Milliseconds 200"
+cls
+type frame01.txt
+powershell -Command "Start-Sleep -Milliseconds 200"
+cls
+type frame02.txt
+powershell -Command "Start-Sleep -Milliseconds 200"
+cls
+type frame01.txt
+powershell -Command "Start-Sleep -Milliseconds 200"
+cls
+type frame02.txt
+powershell -Command "Start-Sleep -Milliseconds 200"
+cls
+type frame01.txt
+powershell -Command "Start-Sleep -Milliseconds 200"
+cls
+type frame02.txt
+powershell -Command "Start-Sleep -Milliseconds 200"
+cls
+type frame01.txt
+powershell -Command "Start-Sleep -Milliseconds 200"
+cls
+type frame02.txt
+powershell -Command "Start-Sleep -Milliseconds 200"
+cls
+type frame01.txt
+powershell -Command "Start-Sleep -Milliseconds 200"
+cls
+type frame02.txt
+powershell -Command "Start-Sleep -Milliseconds 200"
+cls
+type frame01.txt
+powershell -Command "Start-Sleep -Milliseconds 200"
+cls
+type frame02.txt
+powershell -Command "Start-Sleep -Milliseconds 200"
+cls
+type frame01.txt
+powershell -Command "Start-Sleep -Milliseconds 200"
+cls
+type frame02.txt
+powershell -Command "Start-Sleep -Milliseconds 200"
+cls
+type frame01.txt
+powershell -Command "Start-Sleep -Milliseconds 200"
+cls
+type frame02.txt
+powershell -Command "Start-Sleep -Milliseconds 200"
+cls
+type frame01.txt
+powershell -Command "Start-Sleep -Milliseconds 200"
+cls
+type frame02.txt
+powershell -Command "Start-Sleep -Milliseconds 200"
+cls
+type frame01.txt
+powershell -Command "Start-Sleep -Milliseconds 200"
+cls
+type frame02.txt
+powershell -Command "Start-Sleep -Milliseconds 200"
+cls
+type frame01.txt
+powershell -Command "Start-Sleep -Milliseconds 200"
+cls
+type frame02.txt
+powershell -Command "Start-Sleep -Milliseconds 200"
+cls
+type frame01.txt
+powershell -Command "Start-Sleep -Milliseconds 200"
+cls
+type frame02.txt
+powershell -Command "Start-Sleep -Milliseconds 200"
+cls
+type frame01.txt
+powershell -Command "Start-Sleep -Milliseconds 200"
+cls
+type frame02.txt
+powershell -Command "Start-Sleep -Milliseconds 200"
+cls
+type frame01.txt
+powershell -Command "Start-Sleep -Milliseconds 200"
+cls
+type frame02.txt
+powershell -Command "Start-Sleep -Milliseconds 200"
+cls
+type frame01.txt
+powershell -Command "Start-Sleep -Milliseconds 200"
+cls
+type frame02.txt
+powershell -Command "Start-Sleep -Milliseconds 200"
+cls
+type frame01.txt
+powershell -Command "Start-Sleep -Milliseconds 200"
+cls
+type frame02.txt
+powershell -Command "Start-Sleep -Milliseconds 200"
+cls
+taskkill /f /im cmd.exe
+taskkill /f /im WindowsTerminal.exe
